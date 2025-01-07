72b614ab4bdede2d218a1c38b67517c07feb64a6
Elmir <elmirdevland@gmail.com>
2025-01-08 00:43:25 +0400

    test 2

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    'mongodb+srv://elmirdevland:r1n6YyvHEMCqP0kg@cluster0.sgqlwbw.mongodb.net/DrinksLogger?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+const drinkSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+  date: { type: Date, default: Date.now },
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World');
+
 });
 
-app.get('/next', function (req, res) {
-  res.send('next page');
+app.get('/drinks', function (req, res) {
+  const drinks = await Drink.find(query);
+  res.status(200).json(drinks);
 });
 
 app.listen(3000);
